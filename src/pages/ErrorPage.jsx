ed71115a99555acb5d4f97ee0dcb9b4d9b13619a
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 19:46:58 -0500

    Initial commit

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+// import "./errorPage.css"
+
+
+function ErrorPage() {
+    return (
+      <div className="error-page">
+        <h1>{"Oh no, this route doesn't exist!"}</h1>
+        <Link className="home-link" to="/">
+          You can go back to the home page by clicking here, though!
+        </Link>
+      </div>
+    )
+}
+
+export {ErrorPage}
+  
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import "./homePage.css"
+import { Link } from "react-router-dom";
+
+
+function HomePage() {
+    return (
+        <div className="background-container">
+        <div className="home-page">
+            <h1>Welcome to Our Shop!</h1>
+        </div>
+        </div>
+    )
+}
+
+export {HomePage}

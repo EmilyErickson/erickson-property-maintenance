401609760af71cd84709431941681a8de7263d35
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 19:46:38 -0500

    Remove unneeded routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 // import { useState } from 'react';
 import {HomePage} from './pages/HomePage';
-// import {ShopPage} from './pages/ShopPage';
 import { Navbar } from './components/Navbar';
-// import Cart from './components/Cart';
-// import { FetchProducts } from './components/Products';
-// import Checkout from './components/Checkout';
 import {ErrorPage} from "./pages/ErrorPage";
 import { Footer } from './components/Footer';
 
@@ -17,9 +13,6 @@ function App() {
       <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/shop" element={<ShopPage productData={productData} addToCart={addToCart} cartItems={cartItems} removeFromCart={removeFromCart}/>} />
-        <Route path="/cart" element={<Cart cartItems={cartItems} removeFromCart={removeFromCart}/>} />
-        <Route path="/checkout" element={<Checkout cartItems={cartItems} setCartItems={setCartItems}/>} /> */}
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
       </div>

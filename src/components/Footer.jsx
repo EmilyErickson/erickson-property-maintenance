316a0bf565def35b15ab9ad2f2084d7d3575205d
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-19 19:47:13 -0500

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css"
+
+function Footer() {
+  return (
+      <div className="footer">
+        <h3 className="credits">Erickson Property Maintainance </h3>
+      </div>
+  );
+}
+
+
+export {Footer}

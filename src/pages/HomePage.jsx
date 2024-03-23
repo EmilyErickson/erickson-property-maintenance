f18ecb70ff89496e2f2b9fcc1ebee76b51a57fb6
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-23 11:04:36 -0500

    Create home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import "./homePage.css"
-import { Link } from "react-router-dom";
-
+import { Carousel } from "../components/Carousel";
 
 function HomePage() {
     return (
-        <div className="background-container">
-        <div className="home-page">
-            <h1>Welcome to Our Shop!</h1>
-        </div>
-        </div>
+            <div className="home-page">
+                <div className="main-content">
+                    <h2>Family owned and operated</h2>
+                    <p className="skills">We can help you with painting, drywall repair, flooring, bathroom remodels, pressure washing, cleaning, siding, landscaping, small remodels, starlink installation, concrete, and more!</p>
+                    <h3>Shelbyville, IL</h3>
+                </div>
+                <Carousel />
+                <div className="home-about">
+                    <p>Family owned and operated in Shelbyville, we are proud to be known as one of the best property maintenance companies in southern Illinois. <br /> Give us a call or text today to arrange for a FREE quote!</p>
+                </div>
+            </div>
     )
 }
 

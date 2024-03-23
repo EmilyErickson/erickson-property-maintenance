16e0b138c81e65ad6a3c4dbe4dfce0726b6ea932
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-23 11:22:29 -0500

    Create error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom"
-// import "./errorPage.css"
-
 
 function ErrorPage() {
     return (
       <div className="error-page">
-        <h1>{"Oh no, this route doesn't exist!"}</h1>
+        <h1>Oops! It looks like you are lost.</h1>
         <Link className="home-link" to="/">
-          You can go back to the home page by clicking here, though!
+          Maybe you are looking for this page?
         </Link>
       </div>
     )

23b68f945920e2573edb29087ea308a125a6d798
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-23 18:57:21 -0500

    Add link to facebook

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ function HomePage() {
                     <h3>Shelbyville, IL</h3>
                 </div>
                 <Carousel />
+                <p className="facebook-link">Visit us on <a href="https://www.facebook.com/profile.php/?id=100092508355179" target="_blank">Facebook</a> to see what other customers are saying!</p>
                 <div className="home-about">
                     <p>Family owned and operated in Shelbyville, we are proud to be known as one of the best property maintenance companies in southern Illinois. <br /> Give us a call or text today to arrange for a FREE quote!</p>
                 </div>

10c585e63745135f079734a84549fa0bdf366dbe
EmilyErickson <emilyerickson2002@gmail.com>
2024-03-23 11:09:42 -0500

    Create routes to each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-// import { useState } from 'react';
 import {HomePage} from './pages/HomePage';
 import { Navbar } from './components/Navbar';
+import { AboutPage } from './pages/AboutPage';
+import { ContactPage } from './pages/ContactPage';
+import { PortfolioPage } from './pages/PortfolioPage';
+import { LegalPage } from './pages/LegalPage';
 import {ErrorPage} from "./pages/ErrorPage";
 import { Footer } from './components/Footer';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
 
   return (
     <div>
       <Navbar />
-      <div>
+      <div className='main-page-container'>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/legal" element={<LegalPage />} />
         <Route path="/*" element={<ErrorPage />} />
       </Routes>
       </div>
